2ac116048d805a6495c05342bccee97ad82e6cac
Pavel Artemev <damention@Pavels-MacBook-Pro-2.local>
2024-04-27 14:49:57 +0200

    added new -h info, project step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(
         name = "gendiff",
@@ -10,6 +11,12 @@ import picocli.CommandLine.Option;
         mixinStandardHelpOptions = true
 )
 public class App implements Runnable {
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1",paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
