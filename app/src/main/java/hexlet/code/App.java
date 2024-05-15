378e915770553b3f31e887acc98cd46234fcb71b
Pavel Artemev <damention@Pavels-MacBook-Pro-2.local>
2024-05-15 11:24:35 +0200

    added file comparator logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,12 +5,15 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.Map;
+import java.util.concurrent.Callable;
+
 @Command(
         name = "gendiff",
         description = "Compares two configuration files and shows a difference.",
         mixinStandardHelpOptions = true
 )
-public class App implements Runnable {
+public class App implements Callable {
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format;
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
@@ -23,6 +26,19 @@ public class App implements Runnable {
     }
 
     @Override
-    public void run() {
-        System.out.println("Hello World");    }
+    public Object call(){
+            try {
+                String path1 = filepath1;
+                String path2 = filepath2;
+
+                Map<String, Object> map1 = Differ.convert(Differ.parseJson(path1));
+                Map<String, Object> map2 = Differ.convert(Differ.parseJson(path2));
+
+                String diff = Differ.generate(map1, map2);
+                System.out.println(diff);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        return null;
+    }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,46 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Differ {
-    public static void generate (){
+    public static String parseJson(String path) throws Exception {
+        return Files.readString(Paths.get(path));
+    }
+
+    public static Map<String,Object> convert(String JsonContent) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(JsonContent, Map.class);
+    }
+
+    public static String generate (Map<String,Object> map1, Map<String,Object> map2) throws Exception {
+        Map<String,Object> tree = new TreeMap<>();
+
+        for(var key : map1.keySet()) {
+            if(!map2.containsKey(key)) {
+                tree.put(key, "- " + key + ": " + map1.get(key));
+            } else if (!map1.get(key).equals(map2.get(key))) {
+                tree.put(key, "- " + key + ": " + map1.get(key) + "\n  + " + key + ": " + map2.get(key));;
+            } else if ((map1.get(key)).equals(map2.get(key))) {
+                tree.put(key, "  " + key + ": " + map1.get(key));
+            }
+        }
+
+        for (var key : map2.keySet()) {
+            if(!map1.containsKey(key)){
+                tree.put(key, "+ " + key + ": " + map2.get(key));
+            }
+        }
+
+        StringBuilder builder = new StringBuilder("{\n");
+        tree.forEach((key, value) -> builder.append("  " + value +"\n"));
+        builder.append("}");
+
+        return builder.toString();
     }
 }
+
+
